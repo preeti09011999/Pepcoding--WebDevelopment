1841e1281ae127346afa5b3d1cee2c8c1fd26348
Preeti <46680479+preeti09011999@users.noreply.github.com>
2020-04-18 21:59:00 +0530

    Revision

---
+++ Lecture 4/poc/objnmem.js
@@ -0,0 +1,7 @@
+function memchecker(obj){
+    //obj.newProp = "this property was updated by object but the reference was"
+}
+
+let myobj = { name : "myobj"};
+memchecker(myobj);
+console.log(myobj);
