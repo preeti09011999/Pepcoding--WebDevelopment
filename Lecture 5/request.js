65fab36b6f2c208dd6d498a35eb861bf4978a141
Preeti <46680479+preeti09011999@users.noreply.github.com>
2020-04-20 23:23:54 +0530

    Http request

--- Lecture 5/request.js
+++ Lecture 5/request.js
@@ -2,6 +2,7 @@ let request = require("request");
 let fs = require("fs");
 console.log("Before");
 request("http://www.google.com",function (err,res,html) {
+    
     if(err === null && res.statusCode === 200){
         fs.writeFile("index.html",html,function(err){
         console.log("Written file to disk");
