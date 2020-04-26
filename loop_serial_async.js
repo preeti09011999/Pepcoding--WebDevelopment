c3e2d483fce238c8b06a6d7108a84ad8576a0dfe
Preeti <46680479+preeti09011999@users.noreply.github.com>
2020-04-26 22:12:16 +0530

    lp

---
+++ loop_serial_async.js
@@ -0,0 +1,15 @@
+let fs = require("fs");
+let files = ["../f1.txt","../f2.txt","../f3.txt","../f4.txt"];
+readFiles(0);
+//sequentially n number files read
+function readFiles(i){
+    if(i == files.length){
+        return;
+    }
+
+
+fs.readFile(files[i],function(err,data){
+    console.log(`Data of files${i+1} : ${data.byteLength}`);
+    readFiles(i+1);
+})
+}
