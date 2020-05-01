d19e059d09390c8b6b1e62172a4d6e39edde12f3
Preeti <46680479+preeti09011999@users.noreply.github.com>
2020-05-01 21:19:11 +0530

    Scrapping activities

---
+++ Lecture 8/puppeteer.js
@@ -0,0 +1,13 @@
+let puppeteer = require("puppeteer");
+(async function(){
+    //browser open => visible
+    let browser = await puppeteer.launch({
+        headless : false,
+        defaultViewport : null,
+        args : ["--incognito","--start-maximized"]
+    });
+    let pages = await browser.pages();
+    let apge = pages[0];
+    await page.goto("https://www.google.com")
+     
+})();
